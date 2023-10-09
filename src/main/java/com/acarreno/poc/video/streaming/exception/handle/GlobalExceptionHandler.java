6d76ddbc81546c190377e2f40b20f7085aae2c84
Agelvis Carreño <acarreno.effectit@gmail.com>
2023-10-09 13:15:42 +0200

    Adding Handle Exception for Multipart file

--- src/main/java/com/acarreno/poc/video/streaming/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/com/acarreno/poc/video/streaming/exception/handle/GlobalExceptionHandler.java
@@ -1,15 +1,21 @@
 package com.acarreno.poc.video.streaming.exception.handle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
 import com.acarreno.poc.video.streaming.exception.CustomError;
 import com.acarreno.poc.video.streaming.exception.CustomException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+  @Autowired
+  private Environment env;
 
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<CustomError> handleException(CustomException ex) {
@@ -27,6 +33,16 @@ public class GlobalExceptionHandler {
   public ResponseEntity<CustomError> handleException(DataIntegrityViolationException ex) {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CustomError.builder()
         .message(ex.getMessage()).description("Data Integrity Violation").build());
+  }
+
+  @ExceptionHandler(MultipartException.class)
+  public ResponseEntity<CustomError> handleException(MultipartException ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(CustomError.builder().message(ex.getMessage())
+            .description("Video exceeds size limit, configuration limit is "
+                + env.getProperty("spring.servlet.multipart.max-file-size")
+                + ", please change configuration if it is required.")
+            .build());
   }
 
 }
